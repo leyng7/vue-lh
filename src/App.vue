c9a739113c87ec62799515ac6eee0f0836da6a63
unryeong <leyng7@gmail.com>
2021-06-20 23:59:09 +0900

    프록시 연결

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LeaseList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LeaseList from "./components/LeaseList";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LeaseList,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #42b983;
 }
 </style>
---
+++ src/components/LeaseList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="card-container">
+    <div class="card" v-for="notice in notices" v-bind:key="notice.PAN_ID">
+      <div>{{ notice.PAN_SS }} ({{ notice.PAN_NT_ST_DT }} ~ {{ notice.CLSG_DT }})</div>
+      <div>{{ notice.PAN_NM }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "LeaseList",
+  data: function () {
+    return {
+      notices: []
+    }
+  },
+  methods: {
+    getData: () => {
+
+    }
+  },
+  created: function () {
+    const vm = this;
+    axios
+        .get('/api/lhLeaseNoticeInfo')
+        .then(response => {
+          console.log(response.data[0]);
+          vm.notices = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        })
+  }
+}
+</script>
+
+<style scoped>
+#card-container {
+  /*display: flex;
+  justify-content: center;*/
+}
+.card {
+  width: 500px;
+  height: 50px;
+  padding: 5px;
+  border-radius: 0 5px;
+  margin: 0 auto 10px;
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  background-color: white;
+}
+</style>
